be894eada8e7c37dbee05b33d4c68a1fbfd3549d
MaloDrougard <malo.drougard@protonmail.com>
2018-11-12 14:03:45 +0100

    Add only one gtest lib for tigl and creator

--- CreatorLib/CPACSCreatorLibTests/googletest-release-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cfse/Stage_Malo/CPACSCreatorLib/CPACSCreatorLibTests/googletest-release-1.8.0/googletest
-# Build directory: /home/cfse/Stage_Malo/CPACSCreatorLib/CPACSCreatorLibTests/googletest-release-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
